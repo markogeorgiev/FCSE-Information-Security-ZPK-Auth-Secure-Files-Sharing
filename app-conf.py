d0c01f665daea074956b1979129ddbcf7ae44a15
markogeorgiev <markogeorgiev9@gmail.com>
2025-01-22 19:33:32 +0100

    Connect DB

---
+++ app-conf.py
@@ -0,0 +1,3 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:MeremDimka123??Wel0v3MTMommy@db.vfasvopzzkqjqgouwjwv.supabase.co:5432/postgres'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+app.config.from_object('app-conf.Config')
+db = SQLAlchemy(app)
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World!'
 
 
